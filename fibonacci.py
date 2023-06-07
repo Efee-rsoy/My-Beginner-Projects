a973f5cb6fabaf49e741ceeaaf7c8e70a0ecfc24
Efe-Rsoy <eeefeeeersoy@gmail.com>
2023-06-07 22:10:13 +0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    sequence = [0,1]
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+    return sequence
+    
+
+    
+a = fibonacci()
+print(a)
+
